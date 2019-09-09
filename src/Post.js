0782324d3dd26259e444c29ab33362e4f6521756
Ralp Mancawan <rmancawan@innovuze.com>
2019-09-09 14:40:55 +0800

    Implement functions in like and comment button

--- src/Post.js
+++ src/Post.js
@@ -9,7 +9,7 @@ import 'react-image-lightbox/style.css';
 
 import {getPostById} from './DataModels1'
 
-
+import { FaThumbsUp } from 'react-icons/fa';
 
 const axios = require('axios');
 
@@ -25,8 +25,14 @@ class Post extends Component {
 			isOpen: false,
 			imgLightBox: '',
 
-			postID: props.postId
-        }
+			postID: props.postId,
+			isComment: false,
+			afterCommentSpinner: '',
+			isLoadingLike: false,
+			showMoreBtn: 3
+		}
+		
+		this.textComment = this.textComment.bind(this);
 	}
 
 	componentDidMount(){
@@ -35,6 +41,88 @@ class Post extends Component {
 
 	componentDidUpdate(){
 		console.log('componentDidUpdate');
+	}
+
+	textComment(event) {
+		this.setState({
+			commentField: event.target.value,
+		});
+	}
+
+	showMoreBtn = () => {
+		this.setState({ 
+			showMoreBtn: this.state.showMoreBtn + 6 
+		});
+	}
+
+	onComment = () => {
+		this.setState({ isComment: true });
+	}
+
+	onLike = () => {	
+		this.setState({
+			isLoadingLike: true
+		});
+
+		const {postID, postDetails} = this.state;
+		const postData = {
+			id: postID,
+			like: postDetails.like + 1
+		};
+
+		console.log(postData);
+		
+		firebase.database().ref('post/' + postID).update(postData, 
+			(error) => {
+			if (error) {
+				console.log('failed');
+			} else {
+				//on success;
+				this.postD();
+				this.setState({
+					isLoadingLike: false
+				});
+			}
+		});
+	}
+
+	onSubmitComment = (e,id) => {
+		e.preventDefault();
+
+		const {commentField, postDetails} = this.state;
+		let comment;
+
+		this.setState({ afterCommentSpinner: <span className="spinner-grow spinner-grow-sm"></span> })
+
+		if(typeof postDetails.comment === 'string'){
+			comment = [];
+			comment.push(commentField);
+		}else{
+			comment = postDetails.comment;
+			comment.push(commentField);
+		}
+
+		const postData = {
+			id: id,
+			comment: comment
+		};
+
+		firebase.database().ref('post/' + id).update(postData, 
+			(error) => {
+			if (error) {
+				console.log('failed');
+			} else {
+				//on success;
+				this.postD();
+				
+				this.setState({ 
+					getPostLoadingInComment: true,
+					isComment: false,
+					commentField: '',
+					afterCommentSpinner: ''
+				});
+			}
+		});
 	}
 
 	postD = () =>{    
@@ -62,8 +150,8 @@ class Post extends Component {
 	} 
 
 	postDetail = () =>{    
-		let postDetails = this.state.postDetails;
-		let isLoading = this.state.isLoading;
+		//let postDetails = this.state.postDetails;
+		const {postDetails, isLoading, isComment, afterCommentSpinner, isLoadingLike, showMoreBtn} = this.state;
 		let tableDetail;
 
 		if(isLoading === true){
@@ -71,11 +159,66 @@ class Post extends Component {
 		}else{
             tableDetail = 
                 <>
-                <Link className="nav-link" to={'/post/'+postDetails.id}><h2>{ postDetails.title }</h2></Link>
-                <img src={require(''+postDetails.img_path+'')} />
-				<p><b>{ postDetails.author }, { postDetails.date }</b></p>
-                <p>{ postDetails.content }</p>
-				<button>Like</button>
+					<Link className="no-underline" to={'/post/'+postDetails.id}><h2>{ postDetails.title }</h2></Link>
+					<img src={require(''+postDetails.img_path+'')} />
+					<p><b>{ postDetails.author }, { postDetails.date }</b></p>
+					<p>{ postDetails.content }</p>
+					<span>{postDetails.like} <FaThumbsUp /></span>
+
+					<div className="mt-2 mb-2">
+						{isLoadingLike ? (
+							<button className="btn btn-sm btn-primary mr-1" disabled>
+								<span className="spinner-grow spinner-grow-sm"></span> Like
+							</button>
+						) : (
+							<button className="btn btn-sm btn-primary mr-1" onClick={() => this.onLike()}><FaThumbsUp /> Like</button>
+						)}
+
+						<button className="btn btn-sm btn-primary" onClick={()=> this.onComment()}>Comment</button>
+					</div>
+
+					{isComment ? (
+						<div class="mb-5">
+							<form action="GET" >
+								<textarea className="form-control" name="content_value" value={this.state.commentField} onChange={this.textComment}  />
+								<button type="submit" className="btn btn-primary mt-2 float-right" onClick={(e) => this.onSubmitComment(e, postDetails.id)}>
+									{afterCommentSpinner}
+									Save
+								</button>
+							</form>
+						</div>
+					) : null }
+
+					{(typeof postDetails.comment === 'object') ? (
+						/* To display the nested comment */	
+						(postDetails.comment.length <= 3) ? (	
+							<>
+							{postDetails.comment.map((comments,key) => (
+								<div class="card bg-info mb-1" key={key}>
+									<div class="card-body p-2">
+									<strong>User 1</strong> <br/>
+										<p class="card-text">{comments}</p>
+									</div>
+							   	</div>
+							))}
+							</>
+						) : ( 
+							<>
+							{postDetails.comment.slice(0,showMoreBtn).map((comments,key) => (
+								<div class="card bg-info mb-1" key={key}>
+									<div class="card-body p-2">
+									<strong>User 1</strong> <br/>
+										<p class="card-text">{comments}</p>
+									</div>
+							   	</div>	
+							))}
+
+							<button class="btn btn-sm btn-primary" onClick={() => this.showMoreBtn()}>More</button>
+							</>
+						)
+					) : 
+						null
+					}
                 </>
 		}
 		
@@ -89,13 +232,11 @@ class Post extends Component {
 	render() {
 		let postDetail = this.postDetail();
 
-
 		return (
             <>
 				<div className="container-fluid">
                     {postDetail}
                 </div>
-                {/* <h1>Testing</h1> */}
             </>			
 		);
 	}	
